fa6ff0aac294789dd7e18ade013ac97940c9e802
Jeff <45052542+Jeff-thecoder@users.noreply.github.com>
2025-02-03 09:55:48 -0700

    Add project files.

---
+++ MauiViewModel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiViewModel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
